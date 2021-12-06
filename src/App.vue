1179b6517f1258775f6c43220461e2a926ada5a3
994AK <1505398065@qq.com>
2021-12-06 18:48:33 +0800

    首页添加商店类目

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,102 @@
 <template>
-  <div>
-    <div class="wrapper">
-      <div class="wrapper__position">
-        <span class="iconfont wrapper__position__icon">&#xe600;</span>
-        北京理工大学国防科技园2号楼10层
-        <span class="iconfont wrapper__position__notice">&#xe716;</span>
+  <div class="wrapper">
+    <div class="wrapper__position">
+      <span class="iconfont wrapper__position__icon">&#xe600;</span>
+      北京理工大学国防科技园2号楼10层
+      <span class="iconfont wrapper__position__notice">&#xe716;</span>
+    </div>
+    <div class="wrapper__search">
+      <span class="iconfont">&#xe62f;</span>
+      <span class="wrapper__search__text">山姆会员商店优惠商品</span>
+    </div>
+    <div class="wrapper__banner">
+      <img
+        class="wrapper__banner__img"
+        src="https://img.shuicaimi.com/c2020/12/09/0l5qdagaza1.jpg" alt="图片"/>
+    </div>
+    <div class="wrapper__icons">
+      <div class="wrapper__icons__item">
+        <img
+          class="wrapper__icons__item__img"
+          src="./assets/超市.png"
+          alt=""
+        />
+        <p class="wrapper__icons__item__desc">超市便利</p>
+      </div>
+      <div class="wrapper__icons__item">
+        <img
+          class="wrapper__icons__item__img"
+          src="./assets/超市.png"
+          alt=""
+        />
+        <p class="wrapper__icons__item__desc">超市便利</p>
+      </div>
+      <div class="wrapper__icons__item">
+        <img
+          class="wrapper__icons__item__img"
+          src="./assets/超市.png"
+          alt=""
+        />
+        <p class="wrapper__icons__item__desc">超市便利</p>
+      </div>
+      <div class="wrapper__icons__item">
+        <img
+          class="wrapper__icons__item__img"
+          src="./assets/超市.png"
+          alt=""
+        />
+        <p class="wrapper__icons__item__desc">超市便利</p>
+      </div>
+      <div class="wrapper__icons__item">
+        <img
+          class="wrapper__icons__item__img"
+          src="./assets/超市.png"
+          alt=""
+        />
+        <p class="wrapper__icons__item__desc">超市便利</p>
+      </div>
+      <div class="wrapper__icons__item">
+        <img
+          class="wrapper__icons__item__img"
+          src="./assets/超市.png"
+          alt=""
+        />
+        <p class="wrapper__icons__item__desc">超市便利</p>
+      </div>
+      <div class="wrapper__icons__item">
+        <img
+          class="wrapper__icons__item__img"
+          src="./assets/超市.png"
+          alt=""
+        />
+        <p class="wrapper__icons__item__desc">超市便利</p>
+      </div>
+      <div class="wrapper__icons__item">
+        <img
+          class="wrapper__icons__item__img"
+          src="./assets/超市.png"
+          alt=""
+        />
+        <p class="wrapper__icons__item__desc">超市便利</p>
+      </div>
+      <div class="wrapper__icons__item">
+        <img
+          class="wrapper__icons__item__img"
+          src="./assets/超市.png"
+          alt=""
+        />
+        <p class="wrapper__icons__item__desc">超市便利</p>
+      </div>
+      <div class="wrapper__icons__item">
+        <img
+          class="wrapper__icons__item__img"
+          src="./assets/超市.png"
+          alt=""
+        />
+        <p class="wrapper__icons__item__desc">超市便利</p>
       </div>
     </div>
+    <div class="wrapper__gap"></div>
   </div>
   <div class="docker">
     <div class="docker__item docker__item--active">
@@ -27,11 +117,13 @@
       <div class="docker__item__title">我的</div>
     </div>
   </div>
+
 </template>
 
 <style lang="scss">
 @import "./style/viriables.scss";
 @import "./style/mixins.scss";
+
 .wrapper {
   position: absolute;
   left: 0;
@@ -64,15 +156,68 @@
     color: $content-fontcolor;
   }
 
-}
+  &__search {
+    margin-bottom: .12rem;
+    line-height: .32rem;
+    background: #F5F5F5;
+    color: #b7b7b7;
+    border-radius: .16rem;
 
+    .iconfont {
+      display: inline-block;
+      padding: 0 .08rem 0 .16rem;
+      font-size: .16rem;
+    }
+
+    &__text {
+      display: inline-block;
+      font-size: .14rem;
+    }
+  }
+
+  &__banner {
+    &__img {
+      width: 100%;
+      height: 0.86rem;
+    }
+  }
+
+  &__icons {
+    display: flex;
+    flex-wrap: wrap;
+    margin-top: .16rem;
+
+    &__item {
+      width: 20%;
+
+      &__img {
+        display: block;
+        width: .4rem;
+        height: .4rem;
+        margin: 0 auto;
+      }
+
+      &__desc {
+        margin: .06rem 0 .16rem 0;
+        text-align: center;
+        color: $content-fontcolor;
+      }
+    }
+  }
+
+  &__gap {
+    margin:  0 -.18rem;
+    height: .1rem;
+    background: $content-bgColor;
+  }
+}
 .docker {
   display: flex;
   position: absolute;
   bottom: 0;
   width: 100%;
   height: .49rem; //49px
-  border-top: 1px solid #F1F1F1;
+  border-top: 1px solid $content-bgColor;
   color: $content-fontcolor;
 
   //docker__item
