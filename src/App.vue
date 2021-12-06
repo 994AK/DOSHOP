162479d57c6b58bc5782610e22290e7a61e2c7c8
994AK <1505398065@qq.com>
2021-12-06 19:14:05 +0800

    附近店铺已添加,假数据

--- src/App.vue
+++ src/App.vue
@@ -97,6 +97,179 @@
       </div>
     </div>
     <div class="wrapper__gap"></div>
+    <div class="wrapper__nearby">
+      <h3 class="wrapper__nearby__title">附近店铺</h3>
+      <div class="wrapper__nearby__item">
+        <img
+          class="wrapper__nearby__item__img"
+          src="./assets/dummy_56x56_000000_e8968b.png"
+          alt=""
+        />
+        <div class="wrapper__nearby__item__content">
+          <div class="wrapper__nearby__item__content__title">沃尔玛</div>
+          <div class="wrapper__nearby__item__content__tags">
+            <span class="wrapper__nearby__item__content__tags__tag">月售1万+</span>
+            <span class="wrapper__nearby__item__content__tags__tag">月售1万+</span>
+            <span class="wrapper__nearby__item__content__tags__tag">月售1万+</span>
+          </div>
+          <p class="wrapper__nearby__item__content__highlight">VIP满89减4元运费</p>
+        </div>
+
+      </div>
+      <div class="wrapper__nearby__item">
+        <img
+          class="wrapper__nearby__item__img"
+          src="./assets/dummy_56x56_000000_e8968b.png"
+          alt=""
+        />
+        <div class="wrapper__nearby__item__content">
+          <div class="wrapper__nearby__item__content__title">沃尔玛</div>
+          <div class="wrapper__nearby__item__content__tags">
+            <span class="wrapper__nearby__item__content__tags__tag">月售1万+</span>
+            <span class="wrapper__nearby__item__content__tags__tag">月售1万+</span>
+            <span class="wrapper__nearby__item__content__tags__tag">月售1万+</span>
+          </div>
+          <p class="wrapper__nearby__item__content__highlight">VIP满89减4元运费</p>
+        </div>
+
+      </div>
+      <div class="wrapper__nearby__item">
+        <img
+          class="wrapper__nearby__item__img"
+          src="./assets/dummy_56x56_000000_e8968b.png"
+          alt=""
+        />
+        <div class="wrapper__nearby__item__content">
+          <div class="wrapper__nearby__item__content__title">沃尔玛</div>
+          <div class="wrapper__nearby__item__content__tags">
+            <span class="wrapper__nearby__item__content__tags__tag">月售1万+</span>
+            <span class="wrapper__nearby__item__content__tags__tag">月售1万+</span>
+            <span class="wrapper__nearby__item__content__tags__tag">月售1万+</span>
+          </div>
+          <p class="wrapper__nearby__item__content__highlight">VIP满89减4元运费</p>
+        </div>
+
+      </div>
+      <div class="wrapper__nearby__item">
+        <img
+          class="wrapper__nearby__item__img"
+          src="./assets/dummy_56x56_000000_e8968b.png"
+          alt=""
+        />
+        <div class="wrapper__nearby__item__content">
+          <div class="wrapper__nearby__item__content__title">沃尔玛</div>
+          <div class="wrapper__nearby__item__content__tags">
+            <span class="wrapper__nearby__item__content__tags__tag">月售1万+</span>
+            <span class="wrapper__nearby__item__content__tags__tag">月售1万+</span>
+            <span class="wrapper__nearby__item__content__tags__tag">月售1万+</span>
+          </div>
+          <p class="wrapper__nearby__item__content__highlight">VIP满89减4元运费</p>
+        </div>
+
+      </div>
+      <div class="wrapper__nearby__item">
+        <img
+          class="wrapper__nearby__item__img"
+          src="./assets/dummy_56x56_000000_e8968b.png"
+          alt=""
+        />
+        <div class="wrapper__nearby__item__content">
+          <div class="wrapper__nearby__item__content__title">沃尔玛</div>
+          <div class="wrapper__nearby__item__content__tags">
+            <span class="wrapper__nearby__item__content__tags__tag">月售1万+</span>
+            <span class="wrapper__nearby__item__content__tags__tag">月售1万+</span>
+            <span class="wrapper__nearby__item__content__tags__tag">月售1万+</span>
+          </div>
+          <p class="wrapper__nearby__item__content__highlight">VIP满89减4元运费</p>
+        </div>
+
+      </div>
+      <div class="wrapper__nearby__item">
+        <img
+          class="wrapper__nearby__item__img"
+          src="./assets/dummy_56x56_000000_e8968b.png"
+          alt=""
+        />
+        <div class="wrapper__nearby__item__content">
+          <div class="wrapper__nearby__item__content__title">沃尔玛</div>
+          <div class="wrapper__nearby__item__content__tags">
+            <span class="wrapper__nearby__item__content__tags__tag">月售1万+</span>
+            <span class="wrapper__nearby__item__content__tags__tag">月售1万+</span>
+            <span class="wrapper__nearby__item__content__tags__tag">月售1万+</span>
+          </div>
+          <p class="wrapper__nearby__item__content__highlight">VIP满89减4元运费</p>
+        </div>
+
+      </div>
+      <div class="wrapper__nearby__item">
+        <img
+          class="wrapper__nearby__item__img"
+          src="./assets/dummy_56x56_000000_e8968b.png"
+          alt=""
+        />
+        <div class="wrapper__nearby__item__content">
+          <div class="wrapper__nearby__item__content__title">沃尔玛</div>
+          <div class="wrapper__nearby__item__content__tags">
+            <span class="wrapper__nearby__item__content__tags__tag">月售1万+</span>
+            <span class="wrapper__nearby__item__content__tags__tag">月售1万+</span>
+            <span class="wrapper__nearby__item__content__tags__tag">月售1万+</span>
+          </div>
+          <p class="wrapper__nearby__item__content__highlight">VIP满89减4元运费</p>
+        </div>
+
+      </div>
+      <div class="wrapper__nearby__item">
+        <img
+          class="wrapper__nearby__item__img"
+          src="./assets/dummy_56x56_000000_e8968b.png"
+          alt=""
+        />
+        <div class="wrapper__nearby__item__content">
+          <div class="wrapper__nearby__item__content__title">沃尔玛</div>
+          <div class="wrapper__nearby__item__content__tags">
+            <span class="wrapper__nearby__item__content__tags__tag">月售1万+</span>
+            <span class="wrapper__nearby__item__content__tags__tag">月售1万+</span>
+            <span class="wrapper__nearby__item__content__tags__tag">月售1万+</span>
+          </div>
+          <p class="wrapper__nearby__item__content__highlight">VIP满89减4元运费</p>
+        </div>
+
+      </div>
+      <div class="wrapper__nearby__item">
+        <img
+          class="wrapper__nearby__item__img"
+          src="./assets/dummy_56x56_000000_e8968b.png"
+          alt=""
+        />
+        <div class="wrapper__nearby__item__content">
+          <div class="wrapper__nearby__item__content__title">沃尔玛</div>
+          <div class="wrapper__nearby__item__content__tags">
+            <span class="wrapper__nearby__item__content__tags__tag">月售1万+</span>
+            <span class="wrapper__nearby__item__content__tags__tag">月售1万+</span>
+            <span class="wrapper__nearby__item__content__tags__tag">月售1万+</span>
+          </div>
+          <p class="wrapper__nearby__item__content__highlight">VIP满89减4元运费</p>
+        </div>
+
+      </div>
+      <div class="wrapper__nearby__item">
+        <img
+          class="wrapper__nearby__item__img"
+          src="./assets/dummy_56x56_000000_e8968b.png"
+          alt=""
+        />
+        <div class="wrapper__nearby__item__content">
+          <div class="wrapper__nearby__item__content__title">沃尔玛</div>
+          <div class="wrapper__nearby__item__content__tags">
+            <span class="wrapper__nearby__item__content__tags__tag">月售1万+</span>
+            <span class="wrapper__nearby__item__content__tags__tag">月售1万+</span>
+            <span class="wrapper__nearby__item__content__tags__tag">月售1万+</span>
+          </div>
+          <p class="wrapper__nearby__item__content__highlight">VIP满89减4元运费</p>
+        </div>
+
+      </div>
+    </div>
   </div>
   <div class="docker">
     <div class="docker__item docker__item--active">
@@ -125,12 +298,13 @@
 @import "./style/mixins.scss";
 
 .wrapper {
+  overflow-y: auto;
   position: absolute;
   left: 0;
   top: 0;
   bottom: .5rem;
   right: 0;
-  padding: 0 .18rem;
+  padding: 0 .18rem .13rem .18rem;
 
   &__position {
     position: relative;
@@ -206,11 +380,63 @@
   }
 
   &__gap {
-    margin:  0 -.18rem;
+    margin: 0 -.18rem;
     height: .1rem;
     background: $content-bgColor;
   }
+
+  &__nearby {
+    &__title {
+      margin: .16rem 0 .02rem 0;
+      font-size: .18rem;
+      font-weight: normal;
+      color: $content-fontcolor;
+    }
+
+    &__item {
+      display: flex;
+      padding-top: .12rem;
+
+      &__img {
+        margin-right: .16rem;
+        width: .56rem;
+        height: .56rem;
+      }
+
+      &__content {
+        flex: 1;
+        padding-bottom: .12rem;
+        border-bottom: 3px solid $content-bgColor;
+
+        &__title {
+          line-height: .22rem;
+          font-size: .16rem;
+          color: $content-fontcolor;
+        }
+
+        &__tags {
+          margin-top: .08rem;
+          line-height: .18rem;
+          font-size: .13rem;
+          color: $content-fontcolor;
+
+          &__tag {
+            margin-right: .16rem;
+
+          }
+        }
+
+        &__highlight {
+          margin-top: .08rem;
+          line-height: .18rem;
+          font-size: .13rem;
+          color: #E93B3B;
+        }
+      }
+    }
+  }
 }
+
 .docker {
   display: flex;
   position: absolute;
