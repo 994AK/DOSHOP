c5270eb68007defbfed01107bd54c0e9f878f830
994AK <1505398065@qq.com>
2021-12-06 12:28:42 +0800

    构建个线上商店

---
+++ README.md
@@ -0,0 +1,42 @@
+# doshop
+
+## 项目介绍
+
+![Language](https://img.shields.io/badge/language-Vue-9cf)![Language](https://img.shields.io/badge/language-JavaScript-9cf)
+
+🤨项目技术栈: `Vue`+`Vue-Router`+`VueX`
+
+:sparkles:项目功能:**注册与登录**，**首页展示**,**商户详情**，**订单管理**
+
+:sparkles:基于一个**网上商城**,牢固`Vue`编码的项目
+
+浏览器CSS兼容问题
+
+> 使用 `normalize.css`来保证每个浏览器打开样式不会有差异
+
+
+
+
+
+
+
+
+
+## 安装依赖
+
+```
+yarn install
+```
+
+### 启动本次项目
+```
+yarn run serve
+```
+
+### 构建本次项目
+```
+yarn run build
+```
+
+
+
---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="docker">
+    <div class="docker__item docker__item--active">
+      <div class="iconfont">&#xe626;</div>
+      <div class="docker__item__title">首页</div>
+    </div>
+    <div class="docker__item">
+      <div class="iconfont">&#xe62b;</div>
+      <div class="docker__item__title"> 购物车</div>
+    </div>
+    <div class="docker__item">
+      <div class="iconfont">&#xe610;</div>
+      <div class="docker__item__title">订单</div>
+
+    </div>
+    <div class="docker__item">
+      <div class="iconfont">&#xe611;</div>
+      <div class="docker__item__title">我的</div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.docker {
+  display: flex;
+  position: absolute;
+  bottom: 0;
+  //background: red;
+  width: 100%;
+  height: .49rem; //49px
+  border-top: 1px solid #F1F1F1;
+  //docker__item
+  &__item {
+    flex: 1;
+    text-align: center;
+
+    .iconfont {
+      margin: 0.07rem 0 .02rem 0;
+      font-size: .2rem;
+    }
+
+    //docker__item--active
+    &--active {
+      color: #1fa4fc;
+    }
+
+    //docker__item__title
+    &__title {
+      font-size: 20px;
+      transform: scale(0.5, 0.5);
+      transform-origin: center top;
+    }
+  }
+}
+</style>
